5299824b7d730d35a327bbc55733202cbbc6efc1
Janik Hotz <janikhotz@gmail.com>
2020-06-11 12:09:08 +0200

    change formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { NgModule } from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {NgModule} from "@angular/core";
 
-import { AppComponent } from "./app.component";
-import { ScrollFxDirective } from "./scroll-fx.directive";
-import { NavComponent } from "./nav/nav.component";
-import { ContactFormComponent } from "./contact-form/contact-form.component";
-import { InputLabelComponent } from "./input-label/input-label.component";
-import { CheckboxComponent } from "./checkbox/checkbox.component";
-import { ImagesLoadedDirective } from "./images-loaded.directive";
-import { LoadingScreenComponent } from "./loading-screen/loading-screen.component";
-import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
-import { DataSheetComponent } from "./data-sheet/data-sheet.component";
-import { SpectralDiagramComponent } from "./spectral-diagram/spectral-diagram.component";
-import { SpeakerSvgFrontalComponent } from "./speaker-svg-frontal/speaker-svg-frontal.component";
-import { HorizontalLabelComponent } from "./horizontal-label/horizontal-label.component";
-import { VerticalLabelComponent } from "./vertical-label/vertical-label.component";
-import { SpeakerSvgTopComponent } from "./speaker-svg-top/speaker-svg-top.component";
-import { SpeakerSvgSideComponent } from "./speaker-svg-side/speaker-svg-side.component";
-import { SectionDirective } from "./section.directive";
-import { ImprintComponent } from "./imprint/imprint.component";
-import { NotificationsComponent } from './notifications/notifications.component';
+import {AppComponent} from "./app.component";
+import {ScrollFxDirective} from "./scroll-fx.directive";
+import {NavComponent} from "./nav/nav.component";
+import {ContactFormComponent} from "./contact-form/contact-form.component";
+import {InputLabelComponent} from "./input-label/input-label.component";
+import {CheckboxComponent} from "./checkbox/checkbox.component";
+import {ImagesLoadedDirective} from "./images-loaded.directive";
+import {LoadingScreenComponent} from "./loading-screen/loading-screen.component";
+import {ProgressBarComponent} from "./progress-bar/progress-bar.component";
+import {DataSheetComponent} from "./data-sheet/data-sheet.component";
+import {SpectralDiagramComponent} from "./spectral-diagram/spectral-diagram.component";
+import {SpeakerSvgFrontalComponent} from "./speaker-svg-frontal/speaker-svg-frontal.component";
+import {HorizontalLabelComponent} from "./horizontal-label/horizontal-label.component";
+import {VerticalLabelComponent} from "./vertical-label/vertical-label.component";
+import {SpeakerSvgTopComponent} from "./speaker-svg-top/speaker-svg-top.component";
+import {SpeakerSvgSideComponent} from "./speaker-svg-side/speaker-svg-side.component";
+import {SectionDirective} from "./section.directive";
+import {ImprintComponent} from "./imprint/imprint.component";
+import {NotificationsComponent} from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
