84a970ba434c60cf65c5abc1ab1296c611364a03
Janik Hotz <janikhotz@gmail.com>
2020-07-08 18:24:12 +0200

    contact form reformatting

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,14 +6,14 @@ import {
   QueryList,
   AfterViewInit,
   ElementRef
-} from "@angular/core";
+} from "@angular/core"
 
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { NotificationServiceService } from "../notification-service.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import {NotificationServiceService} from "../notification-service.service"
 
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function validateEmail(email: string) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
 }
 
 @Component({
@@ -28,89 +28,87 @@ export class ContactFormComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit() {
-    this.idSuffix = "_" + +new Date();
-
-    (window as any).form = this;
+    this.idSuffix = "_" + +new Date()
   }
 
   ngAfterViewInit() {
-    this.inputs.changes.subscribe(_ => {
-      this.inputs.forEach(input => {
-        let placeholder = input.nativeElement.getAttribute("placeholder");
-        let top = input.nativeElement.offsetTop;
-        let left = input.nativeElement.offsetLeft;
-      });
-    });
+    /* this.inputs.changes.subscribe(_ => { */
+    /*   this.inputs.forEach(input => { */
+    /*     let placeholder = input.nativeElement.getAttribute("placeholder") */
+    /*     let top = input.nativeElement.offsetTop */
+    /*     let left = input.nativeElement.offsetLeft */
+    /*   }) */
+    /* }) */
   }
 
   @HostListener("window:resize")
   documentResized() {}
 
-  submit(event) {
-    event.preventDefault();
-    if (this.data.empty) return "oops robot";
-    this.inputs.map(x => x.nativeElement).forEach(x => console.log(x.name));
+  submit(event: any) {
+    event.preventDefault()
+    if (this.data.empty) return "oops robot"
+    this.inputs.map(x => x.nativeElement).forEach(x => console.log(x.name))
     this.data.agree = this.inputs
       .map(x => x.nativeElement)
-      .find(x => x.name == "data-agree").checked;
-    let error = false;
+      .find(x => x.name == "data-agree").checked
+    let error = false
     if (!this.data.firstname) {
-      this.errors.firstname = "required";
-      error = true;
+      this.errors.firstname = "required"
+      error = true
     }
     if (!this.data.lastname) {
-      this.errors.lastname = "required";
-      error = true;
+      this.errors.lastname = "required"
+      error = true
     }
     if (!this.data.email) {
-      this.errors.email = "required";
-      error = true;
+      this.errors.email = "required"
+      error = true
     } else if (!validateEmail(this.data.email)) {
-      this.errors.email = "invalid";
-      error = true;
+      this.errors.email = "invalid"
+      error = true
     }
     if (!this.data.agree) {
-      this.errors.agree = "required";
+      this.errors.agree = "required"
     }
     if (error) {
-      return;
+      return
     }
-    console.log(this.data);
-    if (!this.data.agree) return;
+    console.log(this.data)
+    if (!this.data.agree) return
     let headers = new HttpHeaders({
       "Content-Type": "application/json"
-    });
+    })
     this.http
       .post(
         "/post-message",
         {
           data: this.data
         },
-        { headers: headers }
+        {headers: headers}
       )
       .subscribe(
         response => {
-          console.log("success", response);
+          console.log("success", response)
           this.notification.open(
             true,
             "Success!",
             "Thanks for getting in touch. I will get back to you as soon as possible."
-          );
+          )
         },
         error => {
-          console.error(error);
+          console.error(error)
           this.notification.open(
             false,
-            "Opps something went wrong!",
+            "Oops something went wrong!",
             "please try again later..."
-          );
+          )
         }
-      );
+      )
   }
 
-  @ViewChildren("input") inputs: QueryList<ElementRef<HTMLInputElement>>;
+  @ViewChildren("input") inputs: QueryList<ElementRef<HTMLInputElement>>
 
-  idSuffix;
+  idSuffix: any
 
   data = {
     firstname: "",
@@ -119,7 +117,7 @@ export class ContactFormComponent implements OnInit, AfterViewInit {
     message: "",
     agree: false,
     empty: ""
-  };
+  }
 
   errors = {
     firstname: null,
@@ -127,5 +125,5 @@ export class ContactFormComponent implements OnInit, AfterViewInit {
     email: null,
     message: null,
     agree: null
-  };
+  }
 }
