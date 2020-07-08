b0da4c21566f0563910fedd967c996b6e859f588
Janik Hotz <janikhotz@gmail.com>
2020-07-08 18:26:40 +0200

    remove some semicolons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import {
   QueryList,
   ViewChild,
 } from "@angular/core";
-import {DomSanitizer} from "@angular/platform-browser";
-import {SectionDirective} from "./section.directive";
-import {NavComponent} from "./nav/nav.component";
+import {DomSanitizer} from "@angular/platform-browser"
+import {SectionDirective} from "./section.directive"
+import {NavComponent} from "./nav/nav.component"
 
-const size_md = 920;
+const size_md = 920
 
 @Component({
   selector: "app-root",
@@ -24,107 +24,107 @@ export class AppComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustStyle(
       `grayscale(${this.tailorTextVR}) ` +
       `brightness(${1 - this.tailorTextVR * 0.5})`
-    );
+    )
   }
   get acousticsImageTransform() {
-    // scale(2) translateX(25%);
+    // scale(2) translateX(25%)
     return this.sanitizer.bypassSecurityTrustStyle(
       `scale(${1 - this.acousticsTextVR * 0.2})` +
       `translateX(${-this.acousticsTextVR * 30}%)`
-    );
+    )
   }
   get acoustics1ImageFilter() {
     return this.sanitizer.bypassSecurityTrustStyle(
       `grayscale(${this.acoustics1TextVR * 0.75}) ` +
       `brightness(${1 - this.acoustics1TextVR * 0.4})`
-    );
+    )
   }
 
   @HostBinding("class.md")
   get isMd() {
-    return this.windowSize == "md";
+    return this.windowSize == "md"
   }
   @HostBinding("class.lg")
   get isLg() {
-    return this.windowSize == "lg";
+    return this.windowSize == "lg"
   }
-  title = "thilobaum";
-  shouldgoout = false;
+  title = "thilobaum"
+  shouldgoout = false
 
-  loadingProgress = 0;
-  imagesLoaded = false;
+  loadingProgress = 0
+  imagesLoaded = false
 
-  datasheetVisible = false;
+  datasheetVisible = false
 
-  tailorTextVR = 0;
+  tailorTextVR = 0
 
-  acousticsTextVR = 0;
+  acousticsTextVR = 0
 
-  acoustics1TextVR = 0;
-  windowSize = "lg";
+  acoustics1TextVR = 0
+  windowSize = "lg"
 
-  @ViewChildren(SectionDirective) sections: QueryList<SectionDirective>;
+  @ViewChildren(SectionDirective) sections: QueryList<SectionDirective>
 
-  hideFoot = false;
-  showImprint: boolean = false;
+  hideFoot = false
+  showImprint: boolean = false
 
-  @ViewChild(NavComponent, {static: true}) nav: NavComponent;
+  @ViewChild(NavComponent, {static: true}) nav: NavComponent
 
   ngOnInit() {
-    this.windowResized();
+    this.windowResized()
     setTimeout(() => {
-      this.shouldgoout = true;
-    }, 5600);
+      this.shouldgoout = true
+    }, 5600)
   }
 
   imagesLoadProgress(event: any) {
-    this.loadingProgress = event;
+    this.loadingProgress = event
   }
   imagesLoadComplete() {
-    console.log("app.component::imagesLoaded");
+    console.log("app.component::imagesLoaded")
     let f = () => {
-      console.log("f, timeout", this.shouldgoout);
+      console.log("f, timeout", this.shouldgoout)
       if (this.shouldgoout) {
-        this.imagesLoaded = true;
+        this.imagesLoaded = true
       } else {
-        setTimeout(f, 100);
+        setTimeout(f, 100)
       }
-    };
-    setTimeout(f, 100);
+    }
+    setTimeout(f, 100)
   }
   toggleDatasheet() {
-    this.datasheetVisible = !this.datasheetVisible;
+    this.datasheetVisible = !this.datasheetVisible
   }
   tailorTextVRChange(event: any) {
-    this.tailorTextVR = event;
+    this.tailorTextVR = event
   }
   acousticsTextVRChange(event: any) {
-    this.acousticsTextVR = event;
+    this.acousticsTextVR = event
   }
   acoustics1TextVRChange(event: any) {
-    this.acoustics1TextVR = event;
+    this.acoustics1TextVR = event
   }
 
   @HostListener("window:resize")
   windowResized() {
-    let w = window.innerWidth;
+    let w = window.innerWidth
     if (w <= size_md) {
-      this.windowSize = "md";
+      this.windowSize = "md"
     } else {
-      this.windowSize = "lg";
+      this.windowSize = "lg"
     }
   }
 
   toggleImprint() {
-    this.showImprint = !this.showImprint;
+    this.showImprint = !this.showImprint
     if (this.showImprint) {
       setTimeout(() => {
-        document.getElementById("imprint").scrollIntoView();
-      }, 200);
+        document.getElementById("imprint").scrollIntoView()
+      }, 200)
     }
   }
 
   arrowClicked() {
-    this.nav.goToSection("aesthetics");
+    this.nav.goToSection("aesthetics")
   }
 }
